d58fbe79ad30e7bdc69256cbf13a2c2345997972
rael23 <israel_persia@yahoo.com>
2020-08-29 20:46:59 +0800

    Updates

---
+++ precache-manifest.2ddc92dcf9dc838503b2f6fb5a31c150.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc4354860c926c35eaf76733deed32f",
+    "url": "/atml/index.html"
+  },
+  {
+    "revision": "51ae95c8463e136632e0",
+    "url": "/atml/static/css/main.0d6b8290.chunk.css"
+  },
+  {
+    "revision": "8f89411575fa33bb5486",
+    "url": "/atml/static/js/2.53667c35.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/atml/static/js/2.53667c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ae95c8463e136632e0",
+    "url": "/atml/static/js/main.8d714d3e.chunk.js"
+  },
+  {
+    "revision": "b0ee6bc385db15de463e",
+    "url": "/atml/static/js/runtime-main.3ecb3304.js"
+  },
+  {
+    "revision": "f6b68a4a147e55a55a8e5db331088afc",
+    "url": "/atml/static/media/atml_logo_60.f6b68a4a.png"
+  },
+  {
+    "revision": "30bf735ce7121c95a1c985f62367b897",
+    "url": "/atml/static/media/tipolo_leaf_trans.30bf735c.png"
+  }
+]);
